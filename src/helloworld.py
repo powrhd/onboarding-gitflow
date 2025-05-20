150f85ca8e0f2047001617decdcc33f0dfad9175
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-05-20 10:57:42 +0200

    feat: include greeting

---
+++ src/helloworld.py
@@ -0,0 +1,11 @@
+def greetings(name: str) -> str:
+    """
+    Function to greet the user with their name.
+
+    Args:
+        name (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    print("Hello, {}!".format(name))
--- src/wrapper.py
+++ src/wrapper.py
@@ -1,8 +1,10 @@
 # The wrapper for calling the transform module and function
 import transform as tr
-
+import helloworld as hw
 
 if __name__ == "__main__":
     r_in = 5.0
     print("Calling the function with r={}".format(r_in))
     r_out = tr.area_circ(r_in)
+    # Greet the user with their name
+    hw.greetings("John")
